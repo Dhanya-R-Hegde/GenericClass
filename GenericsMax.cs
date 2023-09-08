71cc8e9b6364dc5d37db6928029a6695b78cd03d
Dhanya-R-Hegde <1rn19ec037.dhanyarhegde@gmail.com>
2023-09-08 23:45:01 +0530

    [Dhanya R Hegde] : Addede Generics Maximum Value

---
+++ GenericsMax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClasses
+{
+    internal class GenericsMax
+    {
+        public static T MaxValue<T>(T Num1,  T Num2, T Num3) where T : IComparable
+        {
+            if(Num1.CompareTo(Num2)>0 && Num1.CompareTo(Num3)>0 || Num1.CompareTo(Num2) >= 0 && Num1.CompareTo(Num3) > 0 || Num1.CompareTo(Num2) > 0 && Num1.CompareTo(Num3) >= 0)
+                return Num1;
+            if (Num2.CompareTo(Num1) > 0 && Num2.CompareTo(Num3) > 0 || Num2.CompareTo(Num1) >= 0 && Num2.CompareTo(Num3) > 0 || Num2.CompareTo(Num1) > 0 && Num2.CompareTo(Num3) >= 0)
+                return Num2;
+            if (Num3.CompareTo(Num1) > 0 && Num3.CompareTo(Num2) > 0 || Num3.CompareTo(Num1) >= 0 && Num3.CompareTo(Num2) > 0 || Num3.CompareTo(Num1) > 0 && Num3.CompareTo(Num2) >= 0)
+                return Num3;
+            throw new Exception("All the values are same");
+        }
+    }
+}
